e1e8efaad0dc83ca5bc2bea1dd155fc2d855a8ee
Антон Выходцев <vykhodtsevai@gmail.com>
2019-04-23 13:09:10 +0300

    Страница профиля

--- public/v1/js/calculator-page.js
+++ public/v1/js/calculator-page.js
@@ -1,3 +1,9 @@
 ﻿$(document).ready(function () {
-
+    $('input[name="payer_type"]').click(function () {
+        if ($(this).attr("value") == "3rd-person") {
+            $("#3rd-person-payer").show('slow');
+        }else{
+            $("#3rd-person-payer").hide('slow');
+        }
+    });
 });
