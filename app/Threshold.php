8e4ecb8455331a77ca06e9a46dd4b1f18ca92246
Sergey Shlyakhov <sergey.schlyakhov@yandex.ru>
2019-04-23 12:54:52 +0400

    прописыаание секций в админке

--- app/Threshold.php
+++ app/Threshold.php
@@ -6,6 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Threshold extends Model
 {
+
+    protected $fillable = [
+        'rate_id', 'value'
+    ];
     public function rate()
     {
         return $this->hasOne(Type::class, 'id','rate_id');
