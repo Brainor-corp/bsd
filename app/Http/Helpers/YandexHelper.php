b9d059fd96cf7d2cc1534038b4a7c4c5fd66004a
ID-EAST | Артем Солдатов <arty.sold@gmail.com>
2019-04-25 16:51:01 +0400

    WIP: Сохранение заказа

--- app/Http/Helpers/YandexHelper.php
+++ app/Http/Helpers/YandexHelper.php
@@ -7,23 +7,29 @@ class YandexHelper {
     /**
      * Возвращает дистанцию в метрах между двумя точками на карте
      *
+     * В случае ошибки возвращает false
+     *
      * @param $pointFrom
      * @param $pointTo
-     * @return int
+     * @return int|bool
      */
     public static function getDistance($pointFrom, $pointTo) {
-        $distance = 0;
+        $distance = false;
 
         $pointFrom = str_replace(', ', ',', $pointFrom);
         $pointFrom = str_replace(' ', ', ', $pointFrom);
+        $pointFromParts = explode(',', $pointFrom);
+        $pointFrom = "$pointFromParts[1],$pointFromParts[0]";
 
-        $pointTo = str_replace(' ', ',', $pointTo);
-        $pointTo = str_replace(' ', ',', $pointTo);
+        $pointTo = str_replace(', ', ',', $pointTo);
+        $pointTo = str_replace(' ', ', ', $pointTo);
+        $pointToParts = explode(',', $pointTo);
+        $pointTo = "$pointToParts[1],$pointToParts[0]";
 
         $params = [
             'origins' => $pointFrom , // '55.7538127,37.5755189',
             'destinations' => $pointTo, // '55.7489841,37.564189',
-            'mode' => 'transit',
+            'mode' => 'driving',
             'apikey' => env('YANDEX_ROUTE_KEY'),
         ];
         $query = http_build_query($params);
@@ -36,7 +42,11 @@ class YandexHelper {
         $resp = curl_exec($curl);
         curl_close($curl);
 
-        dd(json_decode($resp));
+        $result = json_decode($resp);
+
+        if($result->rows[0]->elements[0]->status === "OK") {
+            return intval($result->rows[0]->elements[0]->distance->value / 1000);
+        }
 
         return $distance;
     }
@@ -44,8 +54,10 @@ class YandexHelper {
     /**
      * Возвращает координаты полученного адреса
      *
+     * В случае ошибки возвращает false
+     *
      * @param $address
-     * @return string
+     * @return string|bool
      */
     public static function getCoordinates($address) {
         $params = [
