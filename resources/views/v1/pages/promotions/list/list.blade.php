5acac0865aabf34793d3713b661e6491ba763762
ID-EAST | Артем Солдатов <arty.sold@gmail.com>
2019-04-27 10:14:51 +0400

    Добавлен вывод акций из базы. Добавлена секция для управления акциями.

--- resources/views/v1/pages/promotions/list/list.blade.php
+++ resources/views/v1/pages/promotions/list/list.blade.php
@@ -27,54 +27,6 @@
                     @endforeach
                 </div>
             @endforeach
-            {{--<div class="row row-item">--}}
-                {{--<div class="col-12 col-sm-6">--}}
-                    {{--<div class="stock__item d-flex flex-column justify-content-center">--}}
-                        {{--<span class="stock__discount d-flex justify-content-center"><span class="amount">5</span><span class="symbol">%</span></span>--}}
-                        {{--<span class="stock__title">Скидка 5% на меж терминальную перевозку груза</span>--}}
-                        {{--<span class="stock__duration"><i class="fa fa-calendar"></i>с 01.05.2016 по 31.08.2016</span>--}}
-                    {{--</div>--}}
-                {{--</div>--}}
-                {{--<div class="col-12 col-sm-6">--}}
-                    {{--<div class="stock__item d-flex flex-column justify-content-center">--}}
-                        {{--<span class="stock__discount d-flex justify-content-center"><span class="amount">5</span><span class="symbol">%</span></span>--}}
-                        {{--<span class="stock__title">Скидка 5% на меж терминальную перевозку груза</span>--}}
-                        {{--<span class="stock__duration"><i class="fa fa-calendar"></i>с 01.05.2016 по 31.08.2016</span>--}}
-                    {{--</div>--}}
-                {{--</div>--}}
-            {{--</div>--}}
-            {{--<div class="row row-item">--}}
-                {{--<div class="col-12 col-sm-6">--}}
-                    {{--<div class="stock__item d-flex flex-column justify-content-center">--}}
-                        {{--<span class="stock__discount d-flex justify-content-center"><span class="amount">5</span><span class="symbol">%</span></span>--}}
-                        {{--<span class="stock__title">Скидка 5% на меж терминальную перевозку груза Скидка 5% на меж терминальную перевозку груза Скидка 5% на меж терминальную перевозку груза</span>--}}
-                        {{--<span class="stock__duration"><i class="fa fa-calendar"></i>с 01.05.2016 по 31.08.2016</span>--}}
-                    {{--</div>--}}
-                {{--</div>--}}
-                {{--<div class="col-12 col-sm-6">--}}
-                    {{--<div class="stock__item d-flex flex-column justify-content-center">--}}
-                        {{--<span class="stock__discount d-flex justify-content-center"><span class="amount">5</span><span class="symbol">%</span></span>--}}
-                        {{--<span class="stock__title">Скидка 5% на меж терминальную перевозку груза</span>--}}
-                        {{--<span class="stock__duration"><i class="fa fa-calendar"></i>с 01.05.2016 по 31.08.2016</span>--}}
-                    {{--</div>--}}
-                {{--</div>--}}
-            {{--</div>--}}
-            {{--<div class="row row-item">--}}
-                {{--<div class="col-12 col-sm-6">--}}
-                    {{--<div class="stock__item d-flex flex-column justify-content-center">--}}
-                        {{--<span class="stock__discount d-flex justify-content-center"><span class="amount">5</span><span class="symbol">%</span></span>--}}
-                        {{--<span class="stock__title">Скидка 5% на меж терминальную перевозку груза</span>--}}
-                        {{--<span class="stock__duration"><i class="fa fa-calendar"></i>с 01.05.2016 по 31.08.2016</span>--}}
-                    {{--</div>--}}
-                {{--</div>--}}
-                {{--<div class="col-12 col-sm-6">--}}
-                    {{--<div class="stock__item d-flex flex-column justify-content-center">--}}
-                        {{--<span class="stock__discount d-flex justify-content-center"><span class="amount">5</span><span class="symbol">%</span></span>--}}
-                        {{--<span class="stock__title">Скидка 5% на меж терминальную перевозку груза</span>--}}
-                        {{--<span class="stock__duration"><i class="fa fa-calendar"></i>с 01.05.2016 по 31.08.2016</span>--}}
-                    {{--</div>--}}
-                {{--</div>--}}
-            {{--</div>--}}
         </div>
     </section>
 @endsection
