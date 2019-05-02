0ec819a644daa3758c6c19fc501fdef0332764bb
ID-EAST | Артем Солдатов <arty.sold@gmail.com>
2019-05-02 11:02:15 +0400

    Добавляем вывод адресов терминалов

--- public/v1/js/map.js
+++ public/v1/js/map.js
@@ -4,66 +4,37 @@ ymaps.ready(function () {
             zoom: 11,
             controls: ['zoomControl', 'fullscreenControl']
         }, {
-            searchControlProvider: 'yandex#search'
+            searchControlProvider: 'yandex#search',
+            maxZoom: 17,
         }),
 
         // Создаём макет содержимого.
         MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
             '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
-        ),
+        );
 
-        // myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
-        //     hintContent: 'Собственный значок метки',
-        //     balloonContent: 'Это красивая метка'
-        // }, {
-        //     // Опции.
-        //     // Необходимо указать данный тип макета.
-        //     iconLayout: 'default#image',
-        //     // Своё изображение иконки метки.
-        //     iconImageHref: 'images/img/logo-map.png',
-        //     // Размеры метки.
-        //     iconImageSize: [40, 40],
-        //     // Смещение левого верхнего угла иконки относительно
-        //     // её "ножки" (точки привязки).
-        //     iconImageOffset: [-5, -38]
-        // }),
+    $('.terminal-block').each(function (key, el) {
+        let point = $(el).data('point').split(', ');
 
-        myPlacemarkWithContent = new ymaps.Placemark([59.93655153819403, 30.350843874102225], {
-            hintContent: 'Безымянный',
-            balloonContent: 'А эта — новогодняя',
-        }, {
-            // Опции.
-            // Необходимо указать данный тип макета.
-            iconLayout: 'default#image',
-            // Своё изображение иконки метки.
-            iconImageHref: 'images/img/logo-map.png',
-            // Размеры метки.
-            iconImageSize: [40, 40],
-            // Смещение левого верхнего угла иконки относительно
-            // её "ножки" (точки привязки).
-            iconImageOffset: [-5, -38]
-        });
+        myMap.geoObjects.add(
+            new ymaps.Placemark([point[1], point[0]], {
+                hintContent: 'Безымянный',
+                balloonContent: 'А эта — новогодняя',
+            }, {
+                // Опции.
+                // Необходимо указать данный тип макета.
+                iconLayout: 'default#image',
+                // Своё изображение иконки метки.
+                iconImageHref: 'images/img/logo-map.png',
+                // Размеры метки.
+                iconImageSize: [40, 40],
+                // Смещение левого верхнего угла иконки относительно
+                // её "ножки" (точки привязки).
+                iconImageOffset: [-5, -38]
+            })
+        );
+    });
 
-        myPlacemarkWithContent1 = new ymaps.Placemark([59.918788761502526, 30.449463335161795], {
-            hintContent: 'Проспект большевиков',
-            balloonContent: 'А эта — новогодняя',
-        }, {
-            // Опции.
-            // Необходимо указать данный тип макета.
-            iconLayout: 'default#image',
-            // Своё изображение иконки метки.
-            iconImageHref: 'images/img/logo-map.png',
-            // Размеры метки.
-            iconImageSize: [40, 40],
-            // Смещение левого верхнего угла иконки относительно
-            // её "ножки" (точки привязки).
-            iconImageOffset: [-5, -38]
-        });
-
-    myMap.geoObjects
-        .add(myPlacemarkWithContent)
-        .add(myPlacemarkWithContent1);
-
-    
     myMap.behaviors.disable('scrollZoom');
+    myMap.setBounds(myMap.geoObjects.getBounds());
 });
