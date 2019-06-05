b5268b11005c417f57584e53fdaae6d27b0e97f9
ID-EAST | Артем Солдатов <arty.sold@gmail.com>
2019-06-05 16:47:13 +0400

    WIP: Восстановление пароля по СМС

--- app/Http/Controllers/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -3,7 +3,12 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Helpers\SMSHelper;
+use App\PasswordResetsPhone;
 use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Support\Facades\Validator;
 
 class ForgotPasswordController extends Controller
 {
@@ -20,6 +25,9 @@ class ForgotPasswordController extends Controller
 
     use SendsPasswordResetEmails;
 
+    // Время жизни кода СМС (в минутах)
+    private $smsCodeLifeTime = 15;
+
     /**
      * Create a new controller instance.
      *
@@ -28,5 +36,62 @@ class ForgotPasswordController extends Controller
     public function __construct()
     {
         $this->middleware('guest');
+    }
+
+    /**
+     * Send a reset link to the given user.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Http\JsonResponse
+     */
+    public function sendResetLinkEmail(Request $request)
+    {
+        if(!empty($request->get('phone'))) { // Восстановление пароля по СМС
+            $request->merge(['phone' => str_replace(array('+', ' ', '(' , ')', '-'), '', $request->phone)]);
+            $validator = $this->validatePhone($request);
+
+            if($validator->fails()){
+                return redirect()->back()->withErrors($validator->errors())->withInput();
+            }
+
+            PasswordResetsPhone::where('phone', $request->get('phone'))->delete();
+
+            $smsCode = rand(100000, 999999);
+            $passwordResetPhone = new PasswordResetsPhone;
+            $passwordResetPhone->phone = $request->get('phone');
+            $passwordResetPhone->code = $smsCode;
+            $passwordResetPhone->save();
+
+            // todo Проверка времени (3 мин)
+            SMSHelper::sendSms($request->get('phone'), $smsCode);
+
+            return redirect(route(''));
+        } elseif(!empty($request->get('email'))) { // Восстановление пароля по номеру телефона
+            $this->validateEmail($request);
+
+            // We will send the password reset link to this user. Once we have attempted
+            // to send the link, we will examine the response then see the message we
+            // need to show to the user. Finally, we'll send out a proper response.
+            $response = $this->broker()->sendResetLink(
+                $this->credentials($request)
+            );
+
+            return $response == Password::RESET_LINK_SENT
+                ? $this->sendResetLinkResponse($request, $response)
+                : $this->sendResetLinkFailedResponse($request, $response);
+        }
+
+        return redirect()->back()->withErrors(['error' => 'Укажите E-Mail или Телефон']);
+    }
+
+    protected function validatePhone(Request $request)
+    {
+        $request->merge(['phone' => str_replace(array('+', ' ', '(' , ')', '-'), '', $request->get('phone'))]);
+
+        $validator = Validator::make($request->all(), [
+            'phone' => ['required', 'regex:/\d{11}/', 'exists:users,phone'],
+        ]);
+
+        return $validator;
     }
 }
--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -17,15 +17,43 @@
                     <form method="POST" action="{{ route('password.email') }}">
                         @csrf
 
+                        @if($errors->has('error'))
+                            <div class="row">
+                                <div class="col-12 text-center">
+                                    <div class="alert alert-danger">
+                                        <strong>{{ $errors->first('error') }}</strong>
+                                    </div>
+                                </div>
+                            </div>
+                        @endif
+
                         <div class="form-group row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail') }}</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
+                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}">
 
                                 @if ($errors->has('email'))
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('или') }}</label>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="phone" class="col-md-4 col-form-label text-md-right">{{ __('Телефон') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="phone" type="text" class="form-control{{ $errors->has('phone') ? ' is-invalid' : '' }} phone-mask" name="phone" value="{{ old('phone') }}">
+
+                                @if ($errors->has('phone'))
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('phone') }}</strong>
                                     </span>
                                 @endif
                             </div>
