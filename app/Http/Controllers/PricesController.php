ae5283b85471189dffe6788c3d85769013c3469e
ID-EAST | Артем Солдатов <arty.sold@gmail.com>
2019-06-11 14:06:59 +0400

    WIP: Поиск цен по городам

--- app/Http/Controllers/PricesController.php
+++ app/Http/Controllers/PricesController.php
@@ -2,27 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\City;
 use App\InsideForwarding;
 use App\Route;
+use Illuminate\Http\Request;
 
 class PricesController extends Controller {
 
-    public function pricesPage() {
-        $shipCity = '53';
-        $destCity = '78';
+    public function pricesPage(Request $request) {
+        $shipCityId = $request->get('ship_city') ?? 53;
+        $destCityId = $request->get('dest_city') ?? 78;
 
         $route = Route::where([
-            ['ship_city_id', $shipCity],
-            ['dest_city_id', $destCity]
+            ['ship_city_id', $shipCityId],
+            ['dest_city_id', $destCityId]
         ])->with('route_tariffs.rate', 'route_tariffs.threshold')->first();
 
         $insideForwardings = InsideForwarding::with('forwardThreshold', 'city')
             ->has('forwardThreshold')
-            ->whereIn('city_id', [$shipCity, $destCity])
+            ->whereIn('city_id', [$shipCityId, $destCityId])
             ->get();
 
+        $shipCities = City::where('is_ship', true)->with('terminal','kladr')->get();
+
 	    return view('v1.pages.prices.prices-page')
-            ->with(compact('route','insideForwardings', 'baseRoute'));
+            ->with(compact('route','insideForwardings', 'shipCities', 'shipCityId', 'destCityId'));
     }
 
 }
--- resources/views/v1/pages/prices/prices-page.blade.php
+++ resources/views/v1/pages/prices/prices-page.blade.php
@@ -16,165 +16,130 @@
                     </header>
                     <div class="row">
                         <div class="col-12">
-                            <h5>Данные маршрута</h5>
-                            <table class="table table-bordered">
-                                <thead>
-                                    <tr>
-                                        <td>Маршрут</td>
-                                        <td>Срок доставки</td>
-                                        <td>Минимальная цена</td>
-                                        <td>Цена бандероли</td>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <td>{{ $route->name }}</td>
-                                        <td>{{ $route->delivery_time }}</td>
-                                        <td>{{ $route->min_cost }}</td>
-                                        <td>{{ $route->wrapper_tariff }}</td>
-                                    </tr>
-                                </tbody>
-                            </table>
+                            <form action="{{ route('pricesPage') }}" method="get">
+                                <div class="form-check form-check-inline">
+                                    <div class="form-group">
+                                        <label for="ship_city">Город отправления:</label>
+                                        <select id="ship_city" class="form-control point-select" name="ship_city" placeholder="Выберите город" required>
+                                            <option value=""></option>
+                                            @foreach($shipCities as $shipCity)
+                                                <option value="{{ $shipCity->id }}" @if($shipCityId == $shipCity->id) selected @endif>{{ $shipCity->name }}</option>
+                                            @endforeach
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="form-check form-check-inline">
+                                    <div class="form-group">
+                                        <label for="dest_city">Город назначения:</label>
+                                        <select id="dest_city" class="form-control point-select" name="dest_city" placeholder="Выберите город" required>
+                                            <option value=""></option>
+                                            @foreach($shipCities as $shipCity)
+                                                <option value="{{ $shipCity->id }}" @if($destCityId == $shipCity->id) selected @endif>{{ $shipCity->name }}</option>
+                                            @endforeach
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="form-check form-check-inline">
+                                    <div class="form-group">
+                                        <label> </label>
+                                        <button type="submit" class="btn margin-item btn-danger form-control">Показать</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12">
+                            @if(isset($route))
+                                <h5>Тарифы на грузоперевозки маршрута {{ $route->name }}</h5>
 
-                            @if(isset($baseRoute))
-                                <h5>Данные базового маршрута</h5>
-                                <table class="table table-bordered">
-                                    <thead>
-                                    <tr>
-                                        <td>Маршрут</td>
-                                        <td>Срок доставки</td>
-                                        <td>Минимальная цена</td>
-                                        <td>Цена бандероли</td>
-                                    </tr>
-                                    </thead>
-                                    <tbody>
-                                    <tr>
-                                        <td>{{ $baseRoute->name }}</td>
-                                        <td>{{ $baseRoute->delivery_time }}</td>
-                                        <td>{{ $baseRoute->min_cost }}</td>
-                                        <td>{{ $baseRoute->wrapper_tariff }}</td>
-                                    </tr>
-                                    </tbody>
-                                </table>
-                            @endif
-                            <h5>Тарифы маршрута</h5>
-                            <table class="table table-bordered">
-                                <thead>
-                                <tr>
-                                    <td>Единица оценки</td>
-                                    <td>Предел</td>
-                                    <td>Цена</td>
-                                </tr>
-                                </thead>
-                                <tbody>
-                                @foreach($route->route_tariffs->groupBy('rate_id') as $routeTariffType)
-                                    @foreach($routeTariffType as $routeTariff)
-                                        <tr>
-                                            <td>{{ $routeTariff->rate->name }}</td>
-                                            <td>{{ $routeTariff->threshold->value }}</td>
-                                            <td>{{ $routeTariff->price }}</td>
-                                        </tr>
-                                    @endforeach
-                                @endforeach
-                                </tbody>
-                            </table>
-                            <h5>Стоимость экспедирования в черте города</h5>
-                            @forelse($insideForwardings->groupBy('city_id') as $insideForwardingsCities)
                                 <div class="table-responsive">
-                                    <table class="table table-bordered">
+                                    <table class="table table-bordered text-center">
                                         <thead>
                                         <tr>
-                                            <th>Город</th>
-                                            @foreach($insideForwardingsCities as $insideForwarding)
+                                            <th rowspan="2">
+                                                <span>Бандероль до 0,01 м3 до 2 кг</span>
+                                            </th>
+                                            <th rowspan="2">
+                                                <span>мин. стоимость руб.</span>
+                                            </th>
+                                            @if($route->route_tariffs->where('rate.slug', 'ves')->count() > 0)
+                                                <th colspan="{{ $route->route_tariffs->where('rate.slug', 'ves')->count() }}">стоимость за 1кг руб.</th>
+                                            @endif
+                                            @if($route->route_tariffs->where('rate.slug', 'obem')->count() > 0)
+                                                <th colspan="{{ $route->route_tariffs->where('rate.slug', 'obem')->count() }}">стоимость за 1м3 руб.</th>
+                                            @endif
+                                            <th rowspan="2">
+                                                <span>мин. срок доставки*</span>
+                                            </th>
+                                        </tr>
+                                        <tr>
+                                            @foreach($route->route_tariffs->where('rate.slug', 'ves') as $routeTariff)
+                                                <th>До {{ $routeTariff->threshold->value }}кг</th>
+                                            @endforeach
+                                            @foreach($route->route_tariffs->where('rate.slug', 'obem') as $routeTariff)
                                                 <th>
-                                                    {{ $insideForwarding->forwardThreshold->name }}
+                                                    До {{ $routeTariff->threshold->value }}м<sup>3</sup>
                                                 </th>
                                             @endforeach
                                         </tr>
                                         </thead>
                                         <tbody>
                                         <tr>
-                                            <td>{{ $insideForwardingsCities->first()->city->name }}</td>
-                                            @foreach($insideForwardingsCities as $insideForwarding)
-                                                <td>
-                                                    {{ $insideForwarding->tariff }}
-                                                </td>
+                                            <td>
+                                                {{ $route->wrapper_tariff }}
+                                            </td>
+                                            <td>
+                                                {{ $route->min_cost }}
+                                            </td>
+                                            @foreach($route->route_tariffs->where('rate.slug', 'ves') as $routeTariff)
+                                                <td>{{ $routeTariff->price }}</td>
                                             @endforeach
+                                            @foreach($route->route_tariffs->where('rate.slug', 'obem') as $routeTariff)
+                                                <td>{{ $routeTariff->price }}</td>
+                                            @endforeach
+                                            <td>
+                                                {{ $route->delivery_time }}
+                                            </td>
                                         </tr>
                                         </tbody>
                                     </table>
                                 </div>
-                                @foreach($insideForwardingsCities as $insideForwarding)
+                                <h5>Стоимость экспедирования в черте города</h5>
+                                @forelse($insideForwardings->groupBy('city_id') as $insideForwardingsCities)
+                                    <div class="table-responsive">
+                                        <table class="table table-bordered text-center">
+                                            <thead>
+                                            <tr>
+                                                <th>Город</th>
+                                                @foreach($insideForwardingsCities as $insideForwarding)
+                                                    <th>
+                                                        {{ $insideForwarding->forwardThreshold->name }}
+                                                    </th>
+                                                @endforeach
+                                            </tr>
+                                            </thead>
+                                            <tbody>
+                                            <tr>
+                                                <td>{{ $insideForwardingsCities->first()->city->name }}</td>
+                                                @foreach($insideForwardingsCities as $insideForwarding)
+                                                    <td>
+                                                        {{ $insideForwarding->tariff }}
+                                                    </td>
+                                                @endforeach
+                                            </tr>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                    @foreach($insideForwardingsCities as $insideForwarding)
 
-                                @endforeach
-                            @empty
-                                <span>Информация отсутствует</span>
-                            @endforelse
-                            {{--<table class="table table-bordered">--}}
-                                {{--<thead>--}}
-                                {{--<tr>--}}
-                                    {{--<td>Город</td>--}}
-                                    {{--@foreach($insideForwardings->pluck('forwardThreshold')->unique() as $forwardThreshold)--}}
-                                        {{--<td>{{ $forwardThreshold->name }}</td>--}}
-                                    {{--@endforeach--}}
-                                {{--</tr>--}}
-                                {{--</thead>--}}
-                                {{--<tbody>--}}
-                                {{--@foreach($insideForwardings->groupBy('city_id') as $insideForwardingCities)--}}
-                                    {{--<tr>--}}
-                                        {{--<td>{{ $insideForwardingCities->first()->city->name }}</td>--}}
-                                        {{--@foreach($insideForwardings->pluck('forwardThreshold')->unique() as $forwardThreshold)--}}
-                                            {{--@php($forward = $insideForwardingCities->where('forwardThreshold.id', $forwardThreshold->id)->first())--}}
-                                            {{--<td>{{ isset($forward) ? $forward->tariff : "" }}</td>--}}
-                                        {{--@endforeach--}}
-                                    {{--</tr>--}}
-                                {{--@endforeach--}}
-                                {{--</tbody>--}}
-                            {{--</table>--}}
-                            {{--<form action="{{ route('show-prices') }}" method="post" class="reports__header row align-items-center">--}}
-                                {{--@csrf--}}
-                                {{--<span class="reports__header-label margin-md-item">Поиск:</span>--}}
-                                {{--<div id="search-wrapper" class="margin-md-item d-flex flex-wrap control-group">--}}
-                                    {{--<select id="search-type-select" class="custom-select">--}}
-                                        {{--<option disabled value="" selected>Выберите из списка</option>--}}
-                                        {{--<option selected value="id">По номеру</option>--}}
-                                        {{--<option value="status">По типу</option>--}}
-                                    {{--</select>--}}
-                                    {{--<input name="id" id="search-input" type="text" class="form-control search-input" placeholder="Введите номер">--}}
-                                {{--</div>--}}
-                                {{--<div id="cb-input" class="custom-control custom-checkbox">--}}
-                                    {{--<input name="finished" value="true" type="checkbox" class="custom-control-input" id="finished-cb">--}}
-                                    {{--<label class="custom-control-label" for="finished-cb">Только завершенные</label>--}}
-                                {{--</div>--}}
-                                {{--<button type="submit" class="btn btn-dotted ml-auto d-flex align-items-center">--}}
-                                    {{--<i class="icons excel-icon margin-item"></i>--}}
-                                    {{--<span class="btn-label margin-item">Выгрузить в excel</span>--}}
-                                {{--</button>--}}
-                            {{--</form>--}}
-                            {{--<div class="row">--}}
-                                {{--<div class="table-responsive">--}}
-                                    {{--<table class="table table-bordered">--}}
-                                        {{--<thead>--}}
-                                        {{--<tr>--}}
-                                            {{--<th>№</th>--}}
-                                            {{--<th>Дата</th>--}}
-                                            {{--<th>Параметры груза</th>--}}
-                                            {{--<th>Город отправителя</th>--}}
-                                            {{--<th>Город получателя</th>--}}
-                                            {{--<th>Отправитель</th>--}}
-                                            {{--<th>Получатель</th>--}}
-                                            {{--<th>Стоимость</th>--}}
-                                            {{--<th>Статус заказа</th>--}}
-                                            {{--<th style="width: 120px">Доступные документы</th>--}}
-                                            {{--<th>&nbsp;</th>--}}
-                                        {{--</tr>--}}
-                                        {{--</thead>--}}
-                                        {{--<tbody id="orders-table-body">--}}
-                                        {{--@include('v1.partials.profile.orders')--}}
-                                        {{--</tbody>--}}
-                                    {{--</table>--}}
-                                {{--</div>--}}
-                            {{--</div>--}}
+                                    @endforeach
+                                @empty
+                                    <span>Информация отсутствует</span>
+                                @endforelse
+                            @else
+                                <span>Выберите город отправления и город назначения</span>
+                            @endif
                         </div>
                     </div>
                 </div>
