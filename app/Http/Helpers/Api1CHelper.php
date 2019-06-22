862750601c61470394e062c6b518e09cc3c0d9f0
ID-EAST | Артем Солдатов <arty.sold@gmail.com>
2019-06-22 15:51:31 +0400

    Добавляем тестовый роут для проверки 1с API

--- app/Http/Helpers/Api1CHelper.php
+++ app/Http/Helpers/Api1CHelper.php
@@ -10,11 +10,13 @@ class Api1CHelper {
         $content = json_encode($params);
 
         $curl = curl_init($url);
-        curl_setopt($curl, CURLOPT_HEADER, false);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($curl, CURLOPT_HTTPHEADER, array("Content-type: application/json"));
-        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "POST");
         curl_setopt($curl, CURLOPT_POSTFIELDS, $content);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+                'Content-Type: application/json',
+                'Content-Length: ' . strlen($content))
+        );
 
         $json_response = curl_exec($curl);
         $status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
