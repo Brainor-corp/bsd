ad10ccf0214ae9c70c12e288ce6ae609aaffd07a
ID-EAST | Артем Солдатов <arty.sold@gmail.com>
2019-07-19 09:16:43 +0400

    Прорабатываем синхронизацию пользователей и заказов

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -3,6 +3,7 @@
 namespace App\Console;
 
 use App\Jobs\OrdersSync;
+use App\Jobs\UsersSync;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -27,8 +28,8 @@ class Kernel extends ConsoleKernel
     {
 //        $schedule->job(new SendTestMail())->everyMinute();
 
-//        $schedule->job(new UsersSync())->everyFiveMinutes();
-        $schedule->job(new OrdersSync())->everyMinute();
+        $schedule->job(new UsersSync())->everyFiveMinutes();
+        $schedule->job(new OrdersSync())->everyFiveMinutes();
     }
 
     /**
