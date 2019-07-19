a6437be15ad11e72378fd63e9892e29bfe3b7037
Sergey Shlyakhov <sergey.schlyakhov@yandex.ru>
2019-07-19 15:41:55 +0400

    работа с документами

--- app/Http/Controllers/ReportsController.php
+++ app/Http/Controllers/ReportsController.php
@@ -139,11 +139,11 @@ class ReportsController extends Controller
                 break;
             case 3:
                 // Заявка на экспедирование
-                $file = DocumentHelper::generateRequestDocument(
+                return DocumentHelper::generateRequestDocument(
                     $documentData,
                     "Заявка №" . $documentData['УникальныйИдентификатор']);
-                $data = json_decode($documentData['ДанныеСайта'], true);
-                dd($documentData);
+//                $data = json_decode($documentData['ДанныеСайта'], true);
+//                dd($documentData);
                 break;
             case 4:
                 // Счет-фактура(УПД???)
