3fb2261b5fd474c9ace7e0874adfeded6e7c40e9
ID-EAST | Артем Солдатов <arty.sold@gmail.com>
2019-07-20 12:01:47 +0400

    WIP: Прорабатываем синхронизацию заказов, добавленных оператором в 1с.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,7 +2,9 @@
 
 namespace App\Console;
 
-use App\Jobs\SendTestMail;
+use App\Jobs\OrdersSyncTo1c;
+use App\Jobs\UsersOrdersSyncFrom1C;
+use App\Jobs\UsersSyncTo1c;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -25,11 +27,11 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->job(new SendTestMail())->everyMinute();
+//        $schedule->job(new SendTestMail())->everyMinute();
 
-//        $schedule->job(new UsersSyncTo1c())->everyFiveMinutes();
-//        $schedule->job(new OrdersSyncTo1c())->everyFiveMinutes();
-//        $schedule->job(new UsersOrdersSyncFrom1C())->everyMinute();
+        $schedule->job(new UsersSyncTo1c())->everyFiveMinutes();
+        $schedule->job(new OrdersSyncTo1c())->everyFiveMinutes();
+        $schedule->job(new UsersOrdersSyncFrom1C())->everyMinute();
     }
 
     /**
