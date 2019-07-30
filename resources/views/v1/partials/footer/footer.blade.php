90aadfada82c16ceb973ee1d811ecf51d34734d7
ID-EAST | Артем Солдатов <arty.sold@gmail.com>
2019-07-30 13:54:32 +0400

    Прорабатываем вывод контактов терминала в футере

--- resources/views/v1/partials/footer/footer.blade.php
+++ resources/views/v1/partials/footer/footer.blade.php
@@ -1,14 +1,26 @@
 <footer class="footer-b bg-dark">
     <div class="container">
         <div class="row justify-content-between">
-            @php($footerMenu = \Zeus\Admin\Cms\Helpers\MenuHelper::getMenuTreeBySlug('menyu-podvala'))
+            @php
+                $footerMenu = \Zeus\Admin\Cms\Helpers\MenuHelper::getMenuTreeBySlug('menyu-podvala')
+            @endphp
             @include('v1.partials.footer.footer-menu', ['nodeTree' => $footerMenu])
             <div class="col-12 col-xl footer-item text-lg-right footer-contacts">
-                <h4 class="whiteTxtColor mb-10"><i class="fa fa-phone"></i>8 (800) 000-00-00</h4>
+                <h4 class="whiteTxtColor mb-10"><i class="fa fa-phone"></i>
+                    @if(isset($closestTerminal->phone))
+                        @php
+                            $phones = preg_split("/(;|,)/", str_replace(' ', '', $closestTerminal->phone));
+                        @endphp
+                        @if(!empty($phones[0]))
+                            {{ $phones[0] }}
+                        @endif
+                    @endif
+                </h4>
                 <p class="darkTxtColor">
-                    198095, г. Санкт-Петербург,<br />
-                    Митрофаньевское <br />
-                    шоссе, д. 10 A</p>
+                    @if(isset($closestTerminal->address))
+                        {{ $closestTerminal->address }}
+                    @endif
+                </p>
                 <div class="d-flex justify-content-lg-end">
                     <a href="##" class="social-link margin-item d-flex justify-content-center align-items-center"><i class="fa fa-vk"></i></a>
                     <a href="##" class="social-link margin-item d-flex justify-content-center align-items-center"><i class="fa fa-facebook"></i></a>
