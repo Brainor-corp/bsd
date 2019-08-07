18bf694a9956333aaaed96963a67a562be1e7b60
Sv1fT <oson-dmitrij@yandex.ru>
2019-08-07 14:15:12 +0400

    Ограничение вводимых символов

--- resources/views/v1/pages/shipment-status/status-page.blade.php
+++ resources/views/v1/pages/shipment-status/status-page.blade.php
@@ -20,7 +20,7 @@
                             <div class="reports__header row align-items-center">
                                 <span class="reports__header-label margin-md-item">Поиск:</span>
                                 <div class="margin-md-item d-flex flex-wrap control-group">
-                                    <input name="order_id" type="text" class="form-control search-input" placeholder="Введите номер" value="{{ request()->get('order_id') }}">
+                                    <input name="order_id" type="text" maxlength="10" class="form-control search-input" placeholder="Введите номер" value="{{ request()->get('order_id') }}">
                                 </div>
                                 <button type="submit" class="btn btn-danger">Найти груз</button>
                             </div>
@@ -30,37 +30,41 @@
             </div>
             <div class="row">
                 <div class="col-12 col-xl-6">
-                    @if(request()->get('order_id'))
-                        @if(isset($order))
-                            <div class="table-responsive">
-                                <table class="table table-bordered">
-                                    <thead>
-                                    <tr>
-                                        <th>№</th>
-                                        <th>Дата доставки</th>
-                                        <th>Статус заказа</th>
-                                    </tr>
-                                    </thead>
-                                    <tbody>
-                                    <tr>
-                                        <td>{{ $order->id }}</td>
-                                        @if(isset($order->estimated_delivery_date))
-                                            <td>
-                                                {{ \Carbon\Carbon::createFromDate($order->estimated_delivery_date)->format('d.m.Y') }}<br><span class="annotation-text">Плановая дата доставки</span>
-                                            </td>
-                                        @else
-                                            <td>
-                                                уточняется
-                                            </td>
-                                        @endif
-                                        <td>{{ $order->status->name }}</td>
-                                    </tr>
-                                    </tbody>
-                                </table>
-                            </div>
-                        @else
-                            Не найдено заказа с таким номером.
+                    @if(strlen(request()->get('order_id')) <= 10)
+                        @if(request()->get('order_id'))
+                            @if(isset($order))
+                                <div class="table-responsive">
+                                    <table class="table table-bordered">
+                                        <thead>
+                                        <tr>
+                                            <th>№</th>
+                                            <th>Дата доставки</th>
+                                            <th>Статус заказа</th>
+                                        </tr>
+                                        </thead>
+                                        <tbody>
+                                        <tr>
+                                            <td>{{ $order->id }}</td>
+                                            @if(isset($order->estimated_delivery_date))
+                                                <td>
+                                                    {{ \Carbon\Carbon::createFromDate($order->estimated_delivery_date)->format('d.m.Y') }}<br><span class="annotation-text">Плановая дата доставки</span>
+                                                </td>
+                                            @else
+                                                <td>
+                                                    уточняется
+                                                </td>
+                                            @endif
+                                            <td>{{ $order->status->name }}</td>
+                                        </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            @else
+                                Не найдено заказа с таким номером.
+                            @endif
                         @endif
+                    @else
+                        <span class="text-danger">Превышено допустимое количество символов.</span>
                     @endif
                 </div>
             </div>
