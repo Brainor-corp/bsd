4ea56a3ed7844bf9ac1c9f8b4d5de0e6acba9312
ID-EAST | Артем Солдатов <arty.sold@gmail.com>
2019-08-21 11:37:14 +0400

    Небольшая правка вёрстки кабинета пользователя

--- resources/views/v1/partials/profile/profile-tabs.blade.php
+++ resources/views/v1/partials/profile/profile-tabs.blade.php
@@ -1,4 +1,4 @@
-<div class="row mb-3">
+<div class="mb-3">
     <div class="nav nav-tabs" id="nav-tab" role="tablist">
         <a class="nav-item nav-link {{ \Request::route()->getName() == 'profile-data-show' ? 'active' : '' }}" id="nav-home-tab" href="{{ route('profile-data-show') }}" role="tab">Профиль</a>
         <a class="nav-item nav-link {{ \Request::route()->getName() == 'profile-balance-show' ? 'active' : '' }}" id="nav-profile-tab" href="{{ route('profile-balance-show') }}" role="tab">Баланс</a>
