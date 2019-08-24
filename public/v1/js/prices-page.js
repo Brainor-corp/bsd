f6ae7d08e810dd3f8607cfb4b8a6e5ae4fccb8d1
ID-EAST | Артем Солдатов <arty.sold@gmail.com>
2019-08-24 13:01:54 +0400

    На странице прайс-листа добавляем сброс значений селектов город по клику на них.

--- public/v1/js/prices-page.js
+++ public/v1/js/prices-page.js
@@ -1,5 +1,10 @@
 $(document).ready(function () {
     $('#ship_city').selectize({
+        onInitialize: function () {
+            this.$control.on("click", function (event) {
+                $('#ship_city').selectize()[0].selectize.clear()
+            });
+        },
         onChange: function(value) {// при изменении города отправления
             if (!value.length) return;
             $.ajaxSetup({
@@ -24,5 +29,11 @@ $(document).ready(function () {
         }
     });
 
-    $('#dest_city').selectize({});
+    $('#dest_city').selectize({
+        onInitialize: function () {
+            this.$control.on("click", function (event) {
+                $('#dest_city').selectize()[0].selectize.clear()
+            });
+        },
+    });
 });
