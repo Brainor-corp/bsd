b38cd90b30c1b1a5b9a5ad196c86e4c4ddcc61ba
ID-EAST | Артем Солдатов <arty.sold@gmail.com>
2019-08-28 13:50:12 +0400

    Прорабатываем отправку данных контрагентов в 1с

--- app/Http/Controllers/Api1cTestController.php
+++ app/Http/Controllers/Api1cTestController.php
@@ -323,44 +323,46 @@ class Api1cTestController extends Controller
             Counterparty::where('id', $request->get('id'))->firstOrFail() :
             Counterparty::orderBy('created_at', 'desc')->first();
 
+        $sendData = [
+            "ПравоваяФорма" => $counterparty->legal_form ?? '',
+            "НаименованиеПолное" => $counterparty->company_name ?? '',
+            "ЮридическоеФизическоеЛицо" => "ФизическоеЛицо",
+            "ИНН" => $counterparty->inn ?? '',
+            "КПП" => $counterparty->kpp ?? '',
+            "ДокументУдостоверяющийЛичность" => intval($counterparty->passport_series . $counterparty->passport_number),
+            "ОсновноеКонтактноеЛицо" => $counterparty->contact_person ?? '',
+            "Комментарий" => $counterparty->addition_info ?? '',
+            "ДатаСоздания" => $counterparty->created_at->format('Y-m-d\TH:i:s'),
+            "ТелефонЗначениеJSON" => [
+                "type" => "Телефон",
+                "value" => intval($counterparty->phone),
+                "CountryCode" => "",
+                "AreaCode" => "",
+                "Number" => $counterparty->phone,
+                "НомерТелефонаБезКодов" => mb_substr($counterparty->phone, -7)
+            ],
+            "ЮридическийАдресЗначениеJSON" => [
+                "type" => "Адрес",
+                "value" => "$counterparty->legal_address_city, $counterparty->legal_address_street, д. $counterparty->legal_address_house, корп. $counterparty->legal_address_block, стр. $counterparty->legal_address_building, кв. $counterparty->legal_address_apartment",
+                "Страна" => "РОССИЯ",
+                "Город" => $counterparty->legal_address_city ?? "",
+                "НомерТелефона" => "",
+                "НомерТелефонаБезКодов" => ""
+            ],
+        ];
         $response1c = \App\Http\Helpers\Api1CHelper::post(
             'new_client',
-            [
-                "ПравоваяФорма" => $counterparty->legal_form ?? '',
-                "НаименованиеПолное" => $counterparty->company_name ?? '',
-                "ЮридическоеФизическоеЛицо" => "ФизическоеЛицо",
-                "ИНН" => $counterparty->inn ?? '',
-                "КПП" => $counterparty->kpp ?? '',
-                "ДокументУдостоверяющийЛичность" => intval($counterparty->passport_series . $counterparty->passport_number),
-                "ОсновноеКонтактноеЛицо" => $counterparty->contact_person ?? '',
-                "Комментарий" => $counterparty->addition_info ?? '',
-                "ДатаСоздания" => $counterparty->created_at->format('Y-m-d\TH:i:s'),
-                "ТелефонЗначениеJSON" => [
-                    "type" => "Телефон",
-                    "value" => intval($counterparty->phone),
-                    "CountryCode" => "",
-                    "AreaCode" => "",
-                    "Number" => $counterparty->phone,
-                    "НомерТелефонаБезКодов" => mb_substr($counterparty->phone, -7)
-                ],
-                "ЮридическийАдресЗначениеJSON" => [
-                    "type" => "Адрес",
-                    "value" => "$counterparty->legal_address_city, $counterparty->legal_address_street, д. $counterparty->legal_address_house, корп. $counterparty->legal_address_block, стр. $counterparty->legal_address_building, кв. $counterparty->legal_address_apartment",
-                    "Страна" => "РОССИЯ",
-                    "Город" => $counterparty->legal_address_city ?? "",
-                    "НомерТелефона" => "",
-                    "НомерТелефонаБезКодов" => ""
-                ],
-            ]
+            $sendData
         );
 
         if(isset($response1c['response']['status']) && $response1c['response']['status'] === "success") {
             $counterparty->code_1c = $response1c['response']['id'];
             $counterparty->save();
-        } else {
-            throw new \Exception(print_r($response1c, 1));
         }
 
-        dd($response1c);
+        dd([
+            'send' => $sendData,
+            'response' => $response1c
+        ]);
     }
 }
