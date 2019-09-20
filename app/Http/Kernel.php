1999a22c58d39fb9a0c4abed2c0b3ab79c06fe0f
ID-EAST | Артем Солдатов <arty.sold@gmail.com>
2019-09-20 09:58:07 +0400

    Исправляем проверку csrf токена в методах api

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -23,11 +23,11 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\TrustProxies::class,
         \Illuminate\Session\Middleware\StartSession::class,
-        \App\Http\Middleware\EncryptCookies::class,
-        \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
-        \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
-        \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-        \App\Http\Middleware\VerifyCsrfToken::class,
+//        \App\Http\Middleware\EncryptCookies::class,
+//        \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
+//        \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+//        \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+//        \App\Http\Middleware\VerifyCsrfToken::class,
     ];
 
     /**
