dd066a2718e85995651c3751c16b350e96c58d22
ID-EAST | Артем Солдатов <arty.sold@gmail.com>
2019-10-02 10:46:29 +0400

    Прорабатываем подсказки для инпута поиска заявки по номеру ЭР

--- public/v1/js/status-page.js
+++ public/v1/js/status-page.js
@@ -1,21 +1,22 @@
 $(document).ready(function () {
     $("input.autocomplete").autocomplete({
         source: function (request, response) {
-            jQuery.get($(this.element).data('source'), {
-                term: request.term,
-            }, function (data) {
-                let result = [];
-                $(data).each(function (key, el) {
-                    result.push(el['cargo_number']);
+            if($('select[name="type"] option:selected').val() === "cargo_number") {
+                jQuery.get($(this.element).data('source'), {
+                    term: request.term,
+                }, function (data) {
+                    let result = [];
+                    $(data).each(function (key, el) {
+                        result.push(el['cargo_number']);
+                    });
+                    response(result);
                 });
-                response(result);
-            });
+            }
         },
         select: function( event, ui ) {
             event.preventDefault();
 
-            console.log(ui.item);
-            console.log($(event.target));
+            $(event.target).val(ui.item.value);
         }
     });
 });
