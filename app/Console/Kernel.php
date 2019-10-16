b2ba70224442890632cecc47132a2c2dfd9c5dad
ID-EAST | Артем Солдатов <arty.sold@gmail.com>
2019-10-16 12:41:21 +0400

    Временно отключаем опросы 1с

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,10 +2,6 @@
 
 namespace App\Console;
 
-use App\Jobs\OrdersSyncTo1c;
-use App\Jobs\SendOrdersPaymentStatusTo1c;
-use App\Jobs\UsersOrdersSyncFrom1c;
-use App\Jobs\UsersSyncTo1c;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -30,10 +26,10 @@ class Kernel extends ConsoleKernel
     {
 //        $schedule->job(new SendTestMail())->everyMinute();
 
-        $schedule->job(new UsersSyncTo1c())->everyMinute();
-        $schedule->job(new OrdersSyncTo1c())->everyMinute();
-        $schedule->job(new UsersOrdersSyncFrom1c())->everyMinute();
-        $schedule->job(new SendOrdersPaymentStatusTo1c())->everyMinute();
+//        $schedule->job(new UsersSyncTo1c())->everyMinute();
+//        $schedule->job(new OrdersSyncTo1c())->everyMinute();
+//        $schedule->job(new UsersOrdersSyncFrom1c())->everyMinute();
+//        $schedule->job(new SendOrdersPaymentStatusTo1c())->everyMinute();
     }
 
     /**
