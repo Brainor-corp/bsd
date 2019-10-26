1700045c527f3e0fc8e5a1d4556d26979215591d
ID-EAST | Артем Солдатов <arty.sold@gmail.com>
2019-10-26 11:57:39 +0400

    WIP: Прорабатываем функционал новой заявки.

--- resources/views/v1/partials/profile/profile-tabs.blade.php
+++ resources/views/v1/partials/profile/profile-tabs.blade.php
@@ -3,5 +3,8 @@
         <a class="nav-item nav-link {{ \Request::route()->getName() == 'profile-data-show' ? 'active' : '' }}" id="nav-home-tab" href="{{ route('profile-data-show') }}" role="tab">Профиль</a>
         <a class="nav-item nav-link {{ \Request::route()->getName() == 'profile-balance-show' ? 'active' : '' }}" id="nav-profile-tab" href="{{ route('profile-balance-show') }}" role="tab">Баланс</a>
         <a class="nav-item nav-link {{ \Request::route()->getName() == 'profile-contract-show' ? 'active' : '' }}" id="nav-contact-tab" href="{{ route('profile-contract-show') }}" role="tab">Договор</a>
+
+        <a class="ml-auto nav-item nav-link border-0" href="{{ route('calculator-show', ['id' => null, 'type' => 'calculator']) }}" >Расчет стоимости</a>
+        <a class="nav-item nav-link border-0" href="{{ route('calculator-show', ['id' => null, 'type' => 'order']) }}" >Оформить новую заявку</a>
     </div>
 </div>
