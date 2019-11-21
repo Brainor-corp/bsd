c05eb329be199b08776437ae1425a799079955fa
sumerk <sumerk.nya@gmail.com>
2019-11-21 15:41:25 +0400

    Прорабатываем pdf заявки

--- app/Http/Helpers/OrderHelper.php
+++ app/Http/Helpers/OrderHelper.php
@@ -2,11 +2,15 @@
 
 namespace App\Http\Helpers;
 
+use Carbon\Carbon;
+
 class OrderHelper {
     public static function orderToPdfData($order) {
         $documentData = [
             'Представление' => "Заявка на перевозку № $order->id",
             'ДатаИсполнения' => $order->order_date ? $order->order_date->format('d.m.Y') : '',
+            'ДатаИсполненияС' => $order->ship_time_from ? Carbon::createFromFormat('H:i:s', $order->ship_time_from)->format('H:i') : '',
+            'ДатаИсполненияПо' => $order->ship_time_to ? Carbon::createFromFormat('H:i:s', $order->ship_time_to)->format('H:i') : '',
             'Груз' => $order->shipping_name,
             'Вес' => $order->total_weight,
             'Объем' => $order->total_volume,
@@ -19,24 +23,28 @@ class OrderHelper {
             'ОрганизацияОтправления' => '', // todo не запрашивается
             'ГрузоотправительТелефон' => $order->sender_phone,
             'АдресЗабора' => $order->take_address ?? "",
-            'РежимРаботыСклада' => '', // todo не запрашивается
+            'РежимРаботыСклада' => 'с 9:00 до 18:00',
             'ГородНазначения' => $order->dest_city_name,
-            'Грузополучатель' => '', // Получается ниже
             'ГрузополучательТелефон' => $order->recipient_phone,
             'АдресДоставки' => $order->delivery_address ?? "",
             'КонтактноеЛицоГрузополучателя' => $order->recipient_contact_person ?? "",
+            'КонтактноеЛицоГрузоотправителя' => $order->sender_contact_person ?? "",
+            'КонтактноеЛицоПлательщика' => '', // Получается ниже
             'АдресГрузополучателя' => $order->recipient_legal_address ?? "",
             'Плательщик' => '', // Получается ниже
             'ФормаОплаты' => $order->payment->name ?? "",
             'Заявку_заполнил' => $order->order_creator,
-            'ПлательщикEmail' => $order->payer_email
+            'ПлательщикEmail' => $order->payer_email,
+            'Комментарий' => $order->cargo_comment,
         ];
 
         if(isset($order->sender_type->name)) {
             if($order->sender_type->slug === 'fizicheskoe-lico') {
                 $documentData['Грузоотправитель'] = $order->sender_name;
             } else {
-                $documentData['Грузоотправитель'] = $order->sender_company_name;
+                $documentData['Грузоотправитель'] = "$order->sender_legal_form \"$order->sender_company_name\"";
+                $documentData['ГрузоотправительИНН'] = $order->sender_inn;
+                $documentData['ГрузоотправительКПП'] = $order->sender_kpp;
             }
         }
 
@@ -44,25 +52,39 @@ class OrderHelper {
             if($order->recipient_type->slug === 'fizicheskoe-lico') {
                 $documentData['Грузополучатель'] = $order->recipient_name;
             } else {
-                $documentData['Грузополучатель'] = $order->recipient_company_name;
+                $documentData['Грузополучатель'] = "$order->recipient_legal_form \"$order->recipient_company_name\"";
+                $documentData['ГрузополучательИНН'] = $order->recipient_inn;
+                $documentData['ГрузополучательКПП'] = $order->recipient_kpp;
             }
         }
 
         switch($order->payer->name) {
             case "Отправитель":
-                $documentData['Плательщик'] = $mapOrder['Грузоотправитель'] ?? '';
+                $documentData['Плательщик'] = $documentData['Грузоотправитель'] ?? '';
+                $documentData['ПлательщикИНН'] = $documentData['ГрузоотправительИНН'] ?? '';
+                $documentData['ПлательщикКПП'] = $documentData['ГрузоотправительКПП'] ?? '';
+                $documentData['ПлательщикТелефон'] = $documentData['ГрузоотправительТелефон'] ?? '';
+                $documentData['КонтактноеЛицоПлательщика'] = $documentData['КонтактноеЛицоГрузоотправителя'] ?? '';
                 break;
 
             case "Получатель":
-                $documentData['Плательщик'] = $mapOrder['Грузополучатель'] ?? '';
+                $documentData['Плательщик'] = $documentData['Грузополучатель'] ?? '';
+                $documentData['ПлательщикИНН'] = $documentData['ГрузополучательИНН'] ?? '';
+                $documentData['ПлательщикКПП'] = $documentData['ГрузополучательКПП'] ?? '';
+                $documentData['ПлательщикТелефон'] = $documentData['ГрузополучательТелефон'] ?? '';
+                $documentData['КонтактноеЛицоПлательщика'] = $documentData['КонтактноеЛицоГрузополучателя'] ?? '';
                 break;
 
             default:
                 if($order->payer_form_type->slug === 'fizicheskoe-lico') {
-                    $mapOrder['Плательщик'] = $order->payer_name ?? "";
+                    $documentData['Плательщик'] = $order->payer_name ?? "";
                 } else {
-                    $mapOrder['Плательщик'] = $order->payer_company_name;
+                    $documentData['Плательщик'] = "$order->payer_legal_form \"$order->payer_company_name\"";
+                    $documentData['ПлательщикИНН'] = $order->payer_inn;
+                    $documentData['ПлательщикКПП'] = $order->payer_kpp;
                 }
+                $documentData['ПлательщикТелефон'] = $order->payer_phone;
+                $documentData['КонтактноеЛицоПлательщика'] = $order->payer_contact_person;
                 break;
         }
 
