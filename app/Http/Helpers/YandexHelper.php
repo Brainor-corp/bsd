08eb04bc2759429b87329e1837e83104259f0ae1
sumerk <sumerk.nya@gmail.com>
2019-12-18 14:17:32 +0400

    Добавляем получение API ключа яндекса из env. Отключаем неиспользуемый функционал матрицы расстояний яндекса.

--- app/Http/Helpers/YandexHelper.php
+++ app/Http/Helpers/YandexHelper.php
@@ -13,43 +13,43 @@ class YandexHelper {
      * @param $pointTo
      * @return int|bool
      */
-    public static function getDistance($pointFrom, $pointTo) {
-        $distance = false;
-
-        $pointFrom = str_replace(', ', ',', $pointFrom);
-        $pointFrom = str_replace(' ', ', ', $pointFrom);
-        $pointFromParts = explode(',', $pointFrom);
-        $pointFrom = "$pointFromParts[1],$pointFromParts[0]";
-
-        $pointTo = str_replace(', ', ',', $pointTo);
-        $pointTo = str_replace(' ', ', ', $pointTo);
-        $pointToParts = explode(',', $pointTo);
-        $pointTo = "$pointToParts[1],$pointToParts[0]";
-
-        $params = [
-            'origins' => $pointFrom , // '55.7538127,37.5755189',
-            'destinations' => $pointTo, // '55.7489841,37.564189',
-            'mode' => 'driving',
-            'apikey' => env('YANDEX_ROUTE_KEY'),
-        ];
-        $query = http_build_query($params);
-
-        $curl = curl_init();
-        curl_setopt_array($curl, [
-            CURLOPT_RETURNTRANSFER => 1,
-            CURLOPT_URL => 'https://api.routing.yandex.net/v1.0.0/distancematrix?' . $query
-        ]);
-        $resp = curl_exec($curl);
-        curl_close($curl);
-
-        $result = json_decode($resp);
-
-        if(isset($result->rows[0]->elements[0]) && $result->rows[0]->elements[0]->status === "OK") {
-            return intval($result->rows[0]->elements[0]->distance->value / 1000);
-        }
-
-        return $distance;
-    }
+//    public static function getDistance($pointFrom, $pointTo) {
+//        $distance = false;
+//
+//        $pointFrom = str_replace(', ', ',', $pointFrom);
+//        $pointFrom = str_replace(' ', ', ', $pointFrom);
+//        $pointFromParts = explode(',', $pointFrom);
+//        $pointFrom = "$pointFromParts[1],$pointFromParts[0]";
+//
+//        $pointTo = str_replace(', ', ',', $pointTo);
+//        $pointTo = str_replace(' ', ', ', $pointTo);
+//        $pointToParts = explode(',', $pointTo);
+//        $pointTo = "$pointToParts[1],$pointToParts[0]";
+//
+//        $params = [
+//            'origins' => $pointFrom , // '55.7538127,37.5755189',
+//            'destinations' => $pointTo, // '55.7489841,37.564189',
+//            'mode' => 'driving',
+//            'apikey' => env('YANDEX_ROUTE_KEY'),
+//        ];
+//        $query = http_build_query($params);
+//
+//        $curl = curl_init();
+//        curl_setopt_array($curl, [
+//            CURLOPT_RETURNTRANSFER => 1,
+//            CURLOPT_URL => 'https://api.routing.yandex.net/v1.0.0/distancematrix?' . $query
+//        ]);
+//        $resp = curl_exec($curl);
+//        curl_close($curl);
+//
+//        $result = json_decode($resp);
+//
+//        if(isset($result->rows[0]->elements[0]) && $result->rows[0]->elements[0]->status === "OK") {
+//            return intval($result->rows[0]->elements[0]->distance->value / 1000);
+//        }
+//
+//        return $distance;
+//    }
 
     /**
      * Возвращает координаты полученного адреса
--- resources/views/v1/partials/header/script.blade.php
+++ resources/views/v1/partials/header/script.blade.php
@@ -1,6 +1,6 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
-<script src="https://api-maps.yandex.ru/2.1/?apikey=40aea2e1-c5cf-4cad-b74d-b81312e190dd&lang=ru_RU" type="text/javascript">
+<script src="https://api-maps.yandex.ru/2.1/?apikey={{ env('YANDEX_GEO_KEY') }}&lang=ru_RU" type="text/javascript">
 </script>
 <script src="{{ asset('/packages/kladrapi-js/jquery.kladr.min.js') }}"></script>
 
