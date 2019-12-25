3d53839e0187d36e4b0e9f3e05441ffdb2786e66
sumerk <sumerk.nya@gmail.com>
2019-12-25 11:06:10 +0400

    Убираем лишние ЦМС пункты из админки

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -28,6 +28,17 @@ class AppServiceProvider extends ServiceProvider
         Date::setLocale('ru');
         Schema::defaultStringLength(191);
 
+        // Убираем лишние ЦМС пункты из админки
+        $removeNavs = ["Записи", "Комментарии", "Метки"];
+        $modifiedPluginData = $this->app['PluginsData'];
+
+        $modifiedPluginData['PluginsNavigation'][0]['nodes'] = array_map(function ($item) use ($removeNavs) {
+            return in_array($item['text'], $removeNavs) ? null : $item;
+        }, $modifiedPluginData['PluginsNavigation'][0]['nodes']);
+
+        $this->app['PluginsData'] = $modifiedPluginData;
+        //
+
         \Illuminate\Support\Collection::macro('recursive', function () {
             return $this->map(function ($value) {
                 if (is_array($value) || is_object($value)) {
