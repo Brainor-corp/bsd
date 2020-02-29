249b1e7e6687b3f7824c1a7423aa68b650865208
sumerk <sumerk.nya@gmail.com>
2020-02-29 13:38:08 +0400

    WIP: Возможность просмотра маршрутов "Из всех городов" и "Во все города" на странице прайс листа.

--- app/Http/Controllers/PricesController.php
+++ app/Http/Controllers/PricesController.php
@@ -11,9 +11,51 @@ use Illuminate\Http\Request;
 
 class PricesController extends Controller {
     public function pricesPage(Request $request) {
-        $shipCityIds = $request->get('ship_city') ?? [53];
-        $destCityIds = $request->get('dest_city') ?? [78];
+        $request->validate([
+            'ship_city' => 'array|between:1,5',
+            'ship_city.*' => 'numeric',
+            'dest_city' => 'array|between:1,5',
+            'dest_city.*' => 'numeric'
+        ]);
 
+        $isShowInPriceOnly = false; // Флаг ограничения выборку маршрутов по полю show_in_price
+        $isToAllAvailable = true; // Флаг доступности пункта "Во всех" для городов назначения
+
+        $shipCityIds = $request->get('ship_city') ?? [53]; // По умолчанию город отправления -- Москва
+        $destCityIds = $request->get('dest_city') ?? [78]; // По умолчанию город Назначения -- Санкт-Петербург
+
+        // Если в городе отправления выбрано "Из всех"
+        if($shipCityIds[0] == 0) {
+            // В качестве городов отправления выберем города, из которых есть маршруты с show_in_price = true
+            $shipCityIds = Route::where('show_in_price', true)
+                ->select('show_in_price', 'ship_city_id')
+                ->get()
+                ->pluck('ship_city_id')
+                ->toArray();
+
+            $isShowInPriceOnly = true;
+            $isToAllAvailable = false;
+        }
+
+        $shipCityIds = array_unique($shipCityIds);
+
+        // Если в городе назначения выбрано "Во все"
+        if($destCityIds[0] == 0) {
+            // В качестве городов назначения выберем города, в которые есть маршруты с show_in_price = true
+            $destCityIds = Route::where('show_in_price', true)
+                // Город отправления маршрутов должен совпадать с одним из городов отправления, выбранных ранее
+                ->whereIn('ship_city_id', $shipCityIds)
+                ->select('show_in_price', 'dest_city_id')
+                ->get()
+                ->pluck('dest_city_id')
+                ->toArray();
+
+            $isShowInPriceOnly = true;
+        }
+
+        $destCityIds = array_unique($destCityIds);
+
+        // Соберем все полученные города в массив пар "Город отправления -> Город назначения"
         $routesIdsPairs = [];
         foreach($shipCityIds as $shipCityId) {
             foreach($destCityIds as $destCityId) {
@@ -24,27 +66,55 @@ class PricesController extends Controller {
             }
         }
 
-        $routes = Route::where(function ($routesQuery) use ($routesIdsPairs) {
-            foreach($routesIdsPairs as $idsPair) {
-                $routesQuery->orWhere($idsPair);
-            }
+        // Если есть пары "Город отправления -> Город назначения", ищем по ним маршруты
+        if(count($routesIdsPairs)) {
+            $routes = Route::where(function ($routesQuery) use ($routesIdsPairs) {
+                foreach($routesIdsPairs as $idsPair) {
+                    $routesQuery->orWhere($idsPair);
+                }
 
-            return $routesQuery;
-        })->with('route_tariffs.rate', 'route_tariffs.threshold')->get();
-
-        $insideForwardings = InsideForwarding::with('forwardThreshold', 'city')
-            ->has('forwardThreshold')
-            ->whereIn('city_id', array_merge($shipCityIds, $destCityIds))
+                return $routesQuery;
+            })
+            ->when($isShowInPriceOnly, function ($routesQuery) {
+                return $routesQuery->where('show_in_price', true);
+            })
+            ->with('route_tariffs.rate', 'route_tariffs.threshold')
             ->get();
+        } else {
+            $routes = null;
+        }
 
-        $perKmTariffs = PerKmTariff::whereIn('tariff_zone_id', $insideForwardings->pluck('city.tariff_zone_id')->unique()->toArray())
-            ->get();
+        $insideForwardings = null;
+        $perKmTariffs = null;
 
+        // Если маршруты найдены
+        if(isset($routes)) {
+            // Найдём тарифы внутренней экспедиции для городов из найденных маршрутов
+            $insideForwardings = InsideForwarding::with('forwardThreshold', 'city')
+                ->has('forwardThreshold')
+                ->whereIn('city_id', array_merge($routes->pluck('ship_city_id')->toArray(), $routes->pluck('dest_city_id')->toArray()))
+                ->get();
+
+            // Найдём покилометровые тарифы
+            $perKmTariffs = PerKmTariff::whereIn('tariff_zone_id', $insideForwardings->pluck('city.tariff_zone_id')->unique()->toArray())
+                ->get();
+        }
+
+        // Просмотр или скачивание
         $action = $request->get('action') ?? 'show';
 
         if($action == 'show') {
             $shipCities = City::where('is_ship', true)->get();
-            $destCities = City::whereIn('id', Route::select(['dest_city_id'])->whereIn('ship_city_id', $shipCityIds))->get();
+
+            if(!empty($request->get('ship_city')) && $request->get('ship_city')[0] == 0) {
+                $destCities = City::whereIn('id', Route::select(['dest_city_id'])->where('show_in_price', true))
+                    ->orderBy('name')
+                    ->get();
+            } else {
+                $destCities = City::whereIn('id', Route::select(['dest_city_id'])->whereIn('ship_city_id', $shipCityIds))
+                    ->orderBy('name')
+                    ->get();
+            }
 
             return view('v1.pages.prices.prices-page')
                 ->with(compact(
@@ -54,7 +124,8 @@ class PricesController extends Controller {
                     'destCities',
                     'shipCityIds',
                     'destCityIds',
-                    'perKmTariffs'
+                    'perKmTariffs',
+                    'isToAllAvailable'
                 ));
         } else {
             $pdf = PDF::loadView('v1.pages.prices.pdf', [
@@ -71,11 +142,25 @@ class PricesController extends Controller {
     }
 
     public function getDestinationCities(Request $request) {
-        $destinationCities = City::whereIn('id', Route::select(['dest_city_id'])->whereIn('ship_city_id', $request->get('ship_city')))
-            ->orderBy('name')
-            ->get();
+        $isToAllAvailable = true;
 
-        return view('v1.partials.prices.destination-cities')->with(compact('destinationCities'));
+        if($request->get('ship_city')[0] == 0) {
+            $destinationCities = City::whereIn('id', Route::select(['dest_city_id'])->where('show_in_price', true))
+                ->orderBy('name')
+                ->get();
+
+            $isToAllAvailable = 0;
+        } else {
+            $destinationCities = City::whereIn('id', Route::select(['dest_city_id'])->whereIn('ship_city_id', $request->get('ship_city')))
+                ->orderBy('name')
+                ->get();
+        }
+
+        return view('v1.partials.prices.destination-cities')
+            ->with(compact(
+                'destinationCities',
+                'isToAllAvailable'
+            ));
     }
 
 }
--- public/v1/js/prices-page.js
+++ public/v1/js/prices-page.js
@@ -1,13 +1,41 @@
+let destSelectParams = {
+    maxItems: 5,
+    plugins: ['remove_button'],
+    onChange: function (value) {
+        if(value.length > 1 && value[value.length - 1] === '0') {
+            $('#dest_city').selectize()[0].selectize.setValue(0);
+        } else if(value.length > 1) {
+            let allIndex = value.indexOf('0');
+            if (allIndex > -1) {
+                value.splice(allIndex, 1);
+                $('#dest_city').selectize()[0].selectize.setValue(value);
+                $('#dest_city').trigger('change');
+            }
+        }
+    }
+};
+
 $(document).ready(function () {
     $('#ship_city').selectize({
+        maxItems: 5,
         plugins: ['remove_button'],
-        // onInitialize: function () {
-        //     this.$control.on("click", function (event) {
-        //         $('#ship_city').selectize()[0].selectize.clear()
-        //     });
-        // },
         onChange: function(value) {// при изменении города отправления
             if (!value.length) return;
+
+            if(value.length > 1 && value[value.length - 1] === '0') {
+                $('#ship_city').selectize()[0].selectize.setValue(0);
+                $('#ship_city').selectize()[0].selectize.updateOption('maxItems', 1);
+                $('#ship_city').trigger('change');
+                return;
+            } else if(value.length > 1) {
+                let allIndex = value.indexOf('0');
+                if (allIndex > -1) {
+                    value.splice(allIndex, 1);
+                    $('#ship_city').selectize()[0].selectize.setValue(value);
+                    $('#ship_city').trigger('change');
+                }
+            }
+
             $.ajaxSetup({
                 headers: {
                     'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
@@ -24,18 +52,11 @@ $(document).ready(function () {
                 success: function(html){
                     $('#dest_city').selectize()[0].selectize.destroy();
                     $('#dest_city').html(html);
-                    $('#dest_city').selectize({});
+                    $('#dest_city').selectize(destSelectParams);
                 }
             });
         }
     });
 
-    $('#dest_city').selectize({
-        plugins: ['remove_button'],
-        // onInitialize: function () {
-        //     this.$control.on("click", function (event) {
-        //         $('#dest_city').selectize()[0].selectize.clear()
-        //     });
-        // },
-    });
+    $('#dest_city').selectize(destSelectParams);
 });
