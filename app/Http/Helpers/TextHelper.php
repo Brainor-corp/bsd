12b54ed4e6d32f03afd7df1d78d8a381af8b9725
Artem Soldatov | Brainor <sumerk.nya@gmail.com>
2020-04-01 16:43:08 +0400

    WIP: Проработка посадочных страниц: проработка блока text_2

--- app/Http/Helpers/TextHelper.php
+++ app/Http/Helpers/TextHelper.php
@@ -3,8 +3,19 @@
 namespace App\Http\Helpers;
 
 class TextHelper {
-    public static function daysTitleByCount($count) {
+    public static function daysTitleByCount($count)
+    {
         $titles = ['дня', 'дней', 'дней'];
+
+        $cases = [2, 0, 1, 1, 1, 2];
+        $format = $titles[($count % 100 > 4 && $count % 100 < 20) ? 2 : $cases[min($count % 10, 5)]];
+
+        return sprintf($format, $count);
+    }
+
+    public static function terminalsTitleByCount($count)
+    {
+        $titles = ['комплекс', 'комплекса', 'комплексов'];
 
         $cases = [2, 0, 1, 1, 1, 2];
         $format = $titles[($count % 100 > 4 && $count % 100 < 20) ? 2 : $cases[min($count % 10, 5)]];
