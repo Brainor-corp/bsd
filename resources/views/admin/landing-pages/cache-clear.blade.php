3705474dc676adefad35cf606f272f6d9f45669c
Artem Soldatov | Brainor <sumerk.nya@gmail.com>
2020-04-02 14:25:57 +0400

    WIP: Проработка посадочных страниц: подсказка про очистку кеша

--- resources/views/admin/landing-pages/cache-clear.blade.php
+++ resources/views/admin/landing-pages/cache-clear.blade.php
@@ -1,5 +1,8 @@
 @if(isset($landingPage))
-    <div class="input-group mb-3">
-        <a href="{{ route('cache-clear', ['key' => $landingPage->url]) }}" class="btn btn-danger">Очистить кеш страницы</a>
+    <div class="alert alert-warning mb-3">
+        Чтобы изменённые данные отразились на странице, необходимо
+        <a href="{{ route('cache-clear', ['key' => $landingPage->url]) }}" class="btn btn-sm btn-danger">
+            очистить кеш страницы
+        </a>
     </div>
 @endif
