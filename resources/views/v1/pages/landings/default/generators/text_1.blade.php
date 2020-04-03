10f85e813bd34d1f7b6e1f3d3ad25ca88c66ead1
Artem Soldatov | Brainor <sumerk.nya@gmail.com>
2020-04-03 14:24:15 +0400

    WIP: Проработка посадочных страниц: стрелочки маршрута

--- resources/views/v1/pages/landings/default/generators/text_1.blade.php
+++ resources/views/v1/pages/landings/default/generators/text_1.blade.php
@@ -1,10 +1,10 @@
-<p style="text-align: center;"><span style="font-size:24px;"><strong>Доставка грузов по маршруту {{ $route->$route->real_arrow_name }}</strong></span></p>
+<p style="text-align: center;"><span style="font-size:24px;"><strong>Доставка грузов по маршруту {{ $route->real_arrow_name }}</strong></span></p>
 
 <p>&nbsp;</p>
 
 <p>
     <span style="font-size:16px;">
-        <span style="color:#5a666e;">Балтийская Служба Доставки осуществляет грузоперевозки по маршруту {{ $route->$route->real_arrow_name }}. </span>
+        <span style="color:#5a666e;">Балтийская Служба Доставки осуществляет грузоперевозки по маршруту {{ $route->real_arrow_name }}. </span>
         <span style="color:#5a666e;">Стоимость доставки груза - от {{ $route->min_cost }} руб.
             <br>Постоянное наличие нескольких машин на маршруте позволяет доставлять товары в точно срок.
             Срок доставки грузов - от {{ $route->delivery_time }} {{ \App\Http\Helpers\TextHelper::daysTitleByCount($route->delivery_time) }}.
