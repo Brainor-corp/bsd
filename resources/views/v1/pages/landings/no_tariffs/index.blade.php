cb5070a3f0cb738f83033d04a0f201a415caa56a
Artem Soldatov | Brainor <sumerk.nya@gmail.com>
2020-06-17 10:12:57 +0400

    На шаблон посадочной страницы возвращаем модальное окно.

--- resources/views/v1/pages/landings/no_tariffs/index.blade.php
+++ resources/views/v1/pages/landings/no_tariffs/index.blade.php
@@ -16,7 +16,43 @@
     @include('v1.pages.landings.no_tariffs.sections.text_2')
     @include('v1.pages.landings.no_tariffs.sections.services')
     @include('v1.pages.landings.no_tariffs.sections.about')
-    @include('v1.pages.landings.no_tariffs.partials.service_modal')
+
+    <!-- Modal -->
+    <div class="modal fade" id="contactModal" tabindex="-1" role="dialog" aria-labelledby="contactModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <form action="{{ route('landing-send-mail') }}" method="post" id="calcUserForm">
+                @csrf
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="contactModalLabel">Получить детальный просчет</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="form-group">
+                            <label for="phone">Ваш телефон</label>
+                            <input type="text" class="form-control" name="phone" id="phone" aria-describedby="phoneHelp" placeholder="Введите телефон" required>
+                            <span class="invalid-feedback error phone" role="alert" style="display: none"></span>
+                        </div>
+                        <div class="form-check">
+                            <input type="checkbox" class="form-check-input" id="check" required>
+                            <label class="form-check-label" for="check">
+                                Даю согласие на обработку <a href="/politika-konfidencialnosti">персональных данных</a>.
+                            </label>
+                        </div>
+                        <div class="form-group">
+                            <div class="g-recaptcha my-3" data-sitekey="{{ env('V2_GOOGLE_CAPTCHA_KEY') }}"></div>
+                            <span class="invalid-feedback error g-recaptcha-response" role="alert" style="display: none"></span>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="submit" class="btn btn-success">Отправить</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
 @endsection
 
 @section('footerScripts')
